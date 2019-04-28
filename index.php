69d0f64ad975e439dcbb1a06112f988b692ad249
JenCat <jc@jencat.pro>
2019-04-28 19:23:11 +0300

    update

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 $host= gethostname();
+switch($host){
+
+}
+$c = curl_init("https://2ip.ru");
+curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+$r = curl_exec($c);
+
+$ipaddr = $r;
+
+
 $ip = gethostbyname($host);
 
-echo "<h1>This is server with IP $ip</h1>";
+echo "<h1>This is server with IP $ipaddr</h1>";
 echo "Server var: ";
 echo "<pre>";
 print_r($_SERVER);
